1c22a27313880c6c601f8435260a0e25c4046fd4
nicola <nicolaccarroll@gmail.com>
2021-08-09 16:27:49 +0100

    marmite is gross

--- app.rb
+++ app.rb
@@ -1,1 +1,14 @@
 require 'sinatra'
+require 'sinatra/reloader' if development?
+
+get '/' do 
+  'Hello world!'
+end
+
+get '/secret' do
+  'Peanut butter is yummy'
+end
+
+get '/secret/truth' do
+  'Marmite is an abomination'
+end
